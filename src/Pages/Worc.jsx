ebc233263953ad87bc2b284e1b6a727f564e4fbf
Sajal pt <ptsajal@gmail.com>
2024-12-10 17:03:34 +0530

    Update Worc.jsx

--- src/Pages/Worc.jsx
+++ src/Pages/Worc.jsx
@@ -1,8 +1,252 @@
 import React from 'react'
+import { Links } from 'react-router-dom';
+import Inst from '../assets/Instagram.png' 
+import Beha from '../assets/Behance.png'
+import Git from '../assets/GitHub.png'
+import Linker from '../assets/LinkedIn.png'
+import Saj from '../assets/sajal.png'
+import bg from '../assets/bg1.jpg'
+import Button from '@mui/material/Button';
+import Vido from '../assets/ss.mp4'
+import Vido1 from '../assets/app.mp4'
+import Vido2 from '../assets/netflix.mp4'
+import Vido3 from '../assets/dryn.mp4'
+import Vido0 from '../assets/ds.mp4' 
+import Work from '../assets/Fram 1.png' 
+import Work2 from '../assets/Group 2-1.jpg' 
+import Work3 from '../assets/Poster.png'
+import Work4 from '../assets/iPhone 16 Pro.png'
+import Work5 from '../assets/Untitled.jpg'
+import Work6 from '../assets/Picsart_24-10-21_12-31-40-908.jpg'
+import Work7 from '../assets/Picsart_24-10-18_06-45-12-945.jpg'
+import Work8 from '../assets/Picsart_24-10-17_17-46-17-628.jpg'
+import Work9 from '../assets/Picsart_24-10-13_12-28-35-086 (1).jpg'
+import Work10 from '../assets/Picsart_24-10-13_12-22-22-169 (1).jpg'
+import Work11 from '../assets/Paper Bag.jpg'
+import Work12 from '../assets/Kraft Paper Postal Bag and Sticker.jpg'
+import Work13 from '../assets/A4 Folder and aper.jpg'
+import Work15 from '../assets/Screenshot 2024-10-22 150833.png'
+import Workl from '../assets/Screenshot 2024-10-22 145646.png'
 
 function Worc() {
   return (
-    <div></div>
+    <>
+    
+        <div id='sajal1' className='container'>
+        <div className='row'>
+            <div className='col-md-6'>
+              <h1 data-aos="fade-right" data-aos-delay="1000" id='werk'>WORKS</h1>
+            </div>
+            <div id='iconn' className='col-md-6  d-flex justify-content-end'>
+           <a href="https://www.instagram.com/sajalpt/?hl=en"><img id='insta' className='ps-3 mt-4' style={{width:"70px"}}   src={Inst}  alt="" /></a>
+           <a href="https://www.behance.net/sajall"> <img id='insta' className='ps-3 mt-4' style={{width:"70px"}} src={Beha} alt="" /></a>
+          <a href="https://github.com/Sajalpt"> <img id='insta' className='ps-3 mt-4' style={{width:"70px"}} src={Git} alt="" /> </a>
+            <a href="https://www.linkedin.com/in/sajalpt/"><img id='insta' className='ps-3 mt-4' style={{width:"70px"}} src={Linker} alt="" /></a>
+           
+         
+
+            </div>
+
+        </div>
+
+
+    </div>
+<div className='container'>
+    <h1 id='demo' className='mt-5'>Demo-Websites</h1>
+  <div className='row mt-5'>
+    <div id='cont1' className='col-md-3'>
+     <h1 id='des'>Description</h1>
+     <p id='para'>Dripin Sneakers is your one-stop e-commerce platform designed with sneaker enthusiasts in mind. Whether you're looking for the latest drops, rare finds, or classic kicks, we’ve got something for every style and passion. Our sleek and intuitive demo design makes it easy to explore and purchase your favorite sneakers.</p>
+    </div>
+    <div className='col-md-6'>
+      <video controls autoplay loop muted  className='img-fluid border' src={Vido}></video>
+    </div>
+    <div id='cont1' className='col-md-3'>
+    <h1 id='des'>Tools used</h1>
+    <ul>
+
+      <li id='para'>Html5</li>
+      <li id='para'>Css3</li>
+      <li id='para'>Bootstrap</li>
+    </ul>
+
+    </div>
+
+  </div>
+
+
+    </div>
+
+    <div className='container'>
+
+  <div className='row mt-5'>
+    <div id='cont1' className='col-md-3'>
+    <h1 id='des'>Description</h1>
+    <p id='para'>This is a demo version of the iconic Apple website, designed to give you a seamless browsing experience just like the original. From sleek product showcases to user-friendly navigation, this clone captures the essence of Apple's minimalistic design and innovative spirit. Explore the features, check out the stunning product layouts, and enjoy a smooth, interactive interface.
+
+ 🍏</p>
+    </div>
+    <div className='col-md-6'>
+      <video controls autoplay loop muted  className='img-fluid border' src={Vido1}></video>
+    </div>
+    <div id='cont1' className='col-md-3'>
+    <h1 id='des'>Tools used</h1>
+    <ul>
+
+<li id='para'>Html5</li>
+<li id='para'>Css3</li>
+<li id='para'>Bootstrap</li>
+</ul>
+    </div>
+
+  </div>
+
+
+    </div>
+    <div className='container'>
+
+  <div className='row mt-5'>
+    <div id='cont1' className='col-md-3'>
+    <h1 id='des'>Description</h1>
+    <p id='para'>This is a demo version of the Netflix website, designed to replicate the look and feel of the popular streaming platform. Explore a vast library of movies, TV shows, and original content, all within a sleek and intuitive interface. This clone showcases features like easy navigation, content categories, and an immersive media player, giving you a taste of the Netflix experience.</p>
+    </div>
+    <div className='col-md-6'>
+      <video controls autoplay loop muted  className='img-fluid border' src={Vido2}></video>
+    </div>
+    <div id='cont1' className='col-md-3'>
+    <h1 id='des'>Tools used</h1>
+    <ul>
+
+<li id='para'>Html5</li>
+<li id='para'>Css3</li>
+<li id='para'>Bootstrap</li>
+</ul>
+    </div>
+
+  </div>
+
+
+    </div>
+    <div className='container'>
+   
+  <div className='row mt-5'>
+    <div id='cont1' className='col-md-3'>
+    <h1 id='des'>Description</h1>
+    <p id='para'>This platform is designed to make shoe care easy and convenient. Whether your favorite sneakers, boots, or dress shoes need a deep clean or a fresh shine, we’re here to help. Our easy-to-use website allows you to schedule pickups, choose cleaning services, and track your order, all from the comfort of your home.</p>
+    </div>
+    <div className='col-md-6'>
+      <video controls autoplay loop muted  className='img-fluid border' src={Vido3}></video>
+    </div>
+    <div id='cont1' className='col-md-3'>
+    <h1 id='des'>Tools used</h1>
+    <ul>
+
+<li id='para'>Html5</li>
+<li id='para'>Css3</li>
+<li id='para'>Bootstrap</li>
+</ul>
+    </div>
+
+  </div>
+
+
+    </div>
+    <div className='container'>
+   
+   <div className='row mt-5'>
+     <div id='cont1' className='col-md-3'>
+     <h1 id='des'>Description</h1>
+     <p id='para'>This website is a simple, responsive landing page built with React, styled with CSS and Bootstrap. The purpose of the website is to emulate the look and feel of a Burger King-themed page, providing users with a clean and modern interface.</p>
+     </div>
+     <div className='col-md-6'>
+       <video controls autoplay loop muted  className='img-fluid border' src={Vido0}></video>
+     </div>
+     <div id='cont1' className='col-md-3'>
+     <h1 id='des'>Tools used</h1>
+     <ul>
+ 
+ <li id='para'>Html5</li>
+ <li id='para'>Css3</li>
+ <li id='para'>Bootstrap</li>
+ <li id='para'>React boostrap</li>
+ <li id='para'>React</li>
+ </ul>
+     </div>
+ 
+   </div>
+ 
+ 
+     </div>
+
+
+     <div id="sect" style={{backgroundColor:""}} className="container-fluid w-100 p-5">
+    <h1 className="text-center" id="about">Other works</h1>
+    <div id="main" className="row p-5">
+        <div className="col-md-6 col-12">
+            <img id="resize" className="img-fluid custom-image" src={Work} alt="Description of Work 1" />
+        </div>
+        <div className="col-md-6 col-12">
+            <img id="resize" className="img-fluid custom-image" src={Work2} alt="Description of Work 2" />
+        </div>
+    </div>
+    <div className="row p-5">
+        <div className="col-md-4 col-12">
+            <img id="resize" className="img-fluid custom-image" src={Work3} alt="Description of Work 3" />
+        </div>
+        <div className="col-md-4 col-12">
+            <img id="resize" className="img-fluid custom-image" src={Work4} alt="Description of Work 4" />
+        </div>
+        <div className="col-md-4 col-12">
+            <img id="resize" className="img-fluid custom-image" src={Work5} alt="Description of Work 5" />
+        </div>
+    </div>
+</div>
+    <div style={{backgroundColor:""}} className="container-fluid w-100 p-5">
+    <div className="row p-5">
+        <div className="col-md-6 col-12">
+          
+        <img id="resize" style={{borderRadius:"10px"}} className="img-fluid" src={Work6} alt="" />
+        </div>
+        <div className="col-md-6 col-12">
+        <img id="resize" style={{borderRadius:"10px"}} className="img-fluid" src={Work7} alt="" />
+        </div>
+    </div>
+
+    <div className="row p-5">
+        <div className="col-md-2 col-12">
+        <img style={{width:"300px",height:"250px",borderRadius:"10px"}} className="img-fluid" src={Work8} alt="" />
+        </div>
+        <div className="col-md-2 col-12">
+        <img style={{width:"300px",height:"250px",borderRadius:"10px"}} className="img-fluid" src={Work9} alt="" />
+        </div>
+        <div className="col-md-2 col-12">
+        <img style={{width:"300px",height:"250px",borderRadius:"10px"}} className="img-fluid" src={Work10} alt="" />
+        </div>
+        <div className="col-md-2 col-12">
+        <img style={{width:"300px",height:"250px",borderRadius:"10px"}} className="img-fluid" src={Work11} alt="" />
+        </div>
+        <div className="col-md-2 col-12">
+          <img style={{width:"300px",height:"250px",borderRadius:"10px"}} className="img-fluid" src={Work12} alt="" />
+          </div>
+        <div className="col-md-2 col-12">
+        <img style={{width:"300px",height:"250px",borderRadius:"10px"}} className="img-fluid" src={Work13} alt="" />
+        </div>
+    </div>
+    </div>
+    <div style={{backgroundColor:"black"}} className="container-fluid w-100 p-5">
+
+    <div className="row p-5">
+        <div className="col-md-6 col-12">
+       <img style={{borderRadius:"10px"}} className="img-fluid" src={Workl} alt="" />
+        </div>
+        <div className="col-md-6 col-12">
+        <img style={{borderRadius:"10px"}} className="img-fluid" src={Work15} alt="" />
+        </div>
+    </div>
+
+    </div>
+    
+    </>
   )
 }
 
